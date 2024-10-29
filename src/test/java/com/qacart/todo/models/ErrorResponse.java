c6979cad3ce077722388d0b5524ceabbd1847cee
Hatem Hatamleh <hatem.hatamleh@gmail.com>
2024-10-29 08:50:33 +0100

    Build the register negative Test case

---
+++ src/test/java/com/qacart/todo/models/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.qacart.todo.models;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ErrorResponse {
+    private String message;
+}
